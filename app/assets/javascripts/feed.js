d8df4a0dc5ff771089b088b00270fa72faffd06d
alemosie <alexandra.siega@gmail.com>
2016-06-03 16:46:34 -0400

    AJAX recursion

--- app/assets/javascripts/feed.js
+++ app/assets/javascripts/feed.js
@@ -2,6 +2,22 @@ function slugify(value){
   return value.toLowerCase().split(" ").join("-")
 }
 
+function getData(url){
+  if (url !== "http://service.dice.com/undefined") {
+    $.getJSON(url, appendFeedItems).done(function(data){
+      getData("http://service.dice.com/" + data.nextUrl) // recursively fetch Dice data that meets criteria
+    })
+  }
+}
+
+function appendFeedItems(response) {
+  var jobs = response.resultItemList
+  jobs.forEach(function(job, index){
+    var item = new FeedItem(job).formatLi()
+    $('#dice-feed').append(item)
+  })
+}
+
 $(function(){
 
   $(':submit').click(function(e){
@@ -10,12 +26,19 @@ $(function(){
 
     var skill = slugify($("#skill").val())
     var zip = $("#zip").val()
-    var url = "http://service.dice.com/api/rest/jobsearch/v1/simple.json?skill=" + skill + "&city=" + zip + "&sort=1"
+    var url = "http://service.dice.com/api/rest/jobsearch/v1/simple.json?skill=" + skill + "&city=" + zip + "&sort=1&age=30"
 
-    $.getJSON(url, function(response){
-      debugger
-    })
-    // 
+    getData(url)
+    // $.getJSON(url, function(response){
+    //   var count = response.count
+    //   var goOn = response.nextUrl
+    //   var jobs = response.resultItemList
+    //   jobs.forEach(function(job){
+    //     item = new FeedItem(job).formatLi()
+    //     $('#dice-feed').append(item)
+    //   })
+    // })
+    //
     // $.ajax({
     //   url: url,
     //   type: 'get',
---
+++ app/assets/javascripts/feedItem.js
@@ -0,0 +1,32 @@
+function FeedItem(json){
+  // distance-from-home
+  // summary
+  // salary
+  // skill-button
+
+  // from AJAX request
+  this.position = json.jobTitle;
+  this.url = json.detailUrl;
+  this.company = json.company;
+  this.datePosted = json.date;
+  this.location = json.location;
+}
+
+FeedItem.prototype.cleanPositionTitle = function(){
+  return this.position.split(/-|\(/)[0].trim()
+}
+
+FeedItem.prototype.formatPosition = function(){
+  return '<h3><a href="' + this.url + '">' + this.cleanPositionTitle() + "</a></h3>"
+}
+
+FeedItem.prototype.formatCompany = function(){
+  return '<h4>' + this.company + "</h4>"
+}
+
+FeedItem.prototype.formatLocation = function(){
+}
+
+FeedItem.prototype.formatLi = function(){
+  return "<div>" + this.formatPosition() + this.formatCompany() + "</div>"
+}
