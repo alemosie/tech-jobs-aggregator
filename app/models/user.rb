354de02b6907886c14ae989dd929c8857dbf74cb
alemosie <alexandra.siega@gmail.com>
2016-06-05 14:56:33 -0400

    add unique & presence validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ActiveRecord::Base
   has_secure_password
 
   has_many :jobs
+  validates_presence_of :name, :username, :email, :password
   
+  validates :username, uniqueness: true
+  validates :email, uniqueness: true
 end
