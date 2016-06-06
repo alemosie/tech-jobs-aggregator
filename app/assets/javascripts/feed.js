4d744a1b5906611f758e5376b3c15e5a5f2f0234
dunxtand <mdrstandish@gmail.com>
2016-06-06 15:38:55 -0400

    minor edits

--- app/assets/javascripts/feed.js
+++ app/assets/javascripts/feed.js
@@ -16,17 +16,6 @@ function addFeedItemSaveButtonListener(){
 function preventForgetButtonPageRefresh(){
   $(":button.forget").click(function(e){
     e.preventDefault();
-}
-
-function addFormSubmitListener() {
-  $('#job-search').submit(function(e){
-    e.preventDefault();
-    e.stopPropagation();
-    var skill = $("#skill").val()
-    var zip = $("#zip").val()
-    var adapter = new DiceAdapter(skill, zip);
-    $("#dice-feed").empty();
-    adapter.getData();
   })
 }
 
