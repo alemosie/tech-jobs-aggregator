dde0f9f8c1a426c5a14474c1e7c64f914cb3fba6
dunxtand <mdrstandish@gmail.com>
2016-06-05 21:04:27 -0400

    clean up feed.js

--- app/assets/javascripts/feed.js
+++ app/assets/javascripts/feed.js
@@ -4,35 +4,36 @@ $(document).ready(function(){
 
 
 function addFormSubmitListener() {
-  $('#job-search').submit(function(e){
-    e.preventDefault();
-    e.stopPropagation();
-
-    var skill = $("#skill").val()
-    var zip = $("#zip").val()
-    var adapter = new DiceAdapter(skill, zip);
-
-    $("#dice-feed").empty();
-    adapter.getData();
-  })
+  $('#job-search').submit(renderJobData);
 }
 
-// this function is called in DiceAdapter.getData(),
-// defined in dice_adapter.js
+// this function is called in DiceAdapter.getData
 function addFeedItemSaveButtonListener(){
-  $(":button.save").click(function(e){
-    e.preventDefault();
-    e.stopPropagation();
+  $(":button.save").click(saveAndRenderJob);
+}
 
-    // change this to check for cookie in browser?
-    if ( $("#saved-jobs-list").length ) {
 
-      var jobInfoDiv = $(this)
-      new Job(jobInfoDiv).save();
+function renderJobData(e){
+  e.preventDefault();
+  e.stopPropagation();
 
-    } else {
-      alert("You must be signed in to do that!");
-    }
+  var skill = $("#skill").val()
+  var zip = $("#zip").val()
+  var adapter = new DiceAdapter(skill, zip);
 
-  })
+  $("#dice-feed").empty();
+  adapter.getData();
+}
+
+function saveAndRenderJob(e){
+  e.preventDefault();
+  e.stopPropagation();
+
+  if ( $("#saved-jobs-list").length ) {
+    var jobInfoDiv = $(this)
+    new Job(jobInfoDiv).save();
+
+  } else {
+    alert("You must be signed in to do that!");
+  }
 }
