e0b9ed5e407f619add7b27f0560bdd8e1b6e2667
dunxtand <mdrstandish@gmail.com>
2016-06-07 00:45:44 -0400

    edits

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in(@user)
-      redirect_to feed_path
+      redirect_to feed_path, notice: "Welcome!"
     else
       redirect_to feed_path, alert: "Invalid submission."
     end
