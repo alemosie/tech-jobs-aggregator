fef1a27d09742ba11ca742c98979ca6b881b7210
dunxtand <mdrstandish@gmail.com>
2016-06-08 09:57:01 -0400

    start adding favicon

--- spec/features/api_search_spec.rb
+++ spec/features/api_search_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-# describe "Searching for job posts" do
+describe "Searching for job posts" do
 
-#   it "renders a job post feed", js: true do
-#     visit feed_path
-#     fill_in "skill", with: "sql"
-#     fill_in "zip", with: "11233"
-#     click_on "Find a job!"
-#     expect(page).to have_content("Found")
-#     expect( first(".job") ).not_to be_nil
-#   end
+  it "renders a job post feed", js: true do
+    visit feed_path
+    fill_in "skill", with: "sql"
+    fill_in "zip", with: "11233"
+    click_on "Find a job!"
+    expect(page).to have_content("Found")
+    expect( first(".job") ).not_to be_nil
+  end
 
-# end
+end
